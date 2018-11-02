7ff827bce99f1f2b987346a8637a2b801b2b372d
Leonora <leonorap@kth.se>
2018-11-02 20:08:35 +0100

    add search by type or name

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -3,12 +3,46 @@ import PokemonCard from './PokemonCard';
 import { PokemonListStyle } from './PokemonList.module.css';
 
 class PokemonList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pokemonList: [].concat(this.props.Items),
+    };
+
+    this.onTextSearch = this.onTextSearch.bind(this);
+  }
+
+  onTextSearch(e) {
+    let input = e.target.value.trim().toLowerCase();
+    let filteredList = this.state.pokemonList.filter(p => {
+      let name = p.name.toLowerCase();
+      let hasType = p.type.filter(pokemonType => {
+        return pokemonType.toLowerCase().startsWith(input);
+      });
+      return name.startsWith(input) || hasType.length > 0;
+    });
+
+    this.setState({
+      pokemonList: filteredList,
+    });
+  }
+
   render() {
     return (
-      <div className={PokemonListStyle}>
-        {this.props.Items.map(pokemon => {
-          return <PokemonCard key={pokemon.id} Item={pokemon} />;
-        })}
+      <div>
+        <div>
+          <input
+            placeholder="Search pokemon by type or name"
+            type="text"
+            onKeyUp={this.onTextSearch}
+          />
+        </div>
+        <div className={PokemonListStyle}>
+          {this.state.pokemonList.map(pokemon => {
+            return <PokemonCard key={pokemon.id} Item={pokemon} />;
+          })}
+        </div>
       </div>
     );
   }
